86485455854125f0d6f969828c3cb1425685573f
zah <zaharid@gmail.com>
2014-05-16 13:25:18 +0200

    Removed and list widget
    Don't believe they are worth the effort now.
    They are in branch "Elementwidget"

--- widgets.py
+++ widgets.py
@@ -19,39 +19,3 @@ widget_mapping = defaultdict(lambda: widgets.TextWidget, {
 
 })
 
-class ListWidget(widgets.ContainerWidget):
-
-    value = List(sync=True)
-    description = Unicode(help="Description of the value this widget represents", sync=True)
-    title = Unicode(value = 'List', sync=True)
-    def __init__(self, *args, **kwargs):
-        super(ListWidget, self).__init__(*args, **kwargs)
-        self._title_label = widgets.LatexWidget(value = self.title)
-        self._item_container = widgets.ContainerWidget()
-
-    def _value_changed(self, name, old, new):
-        self.add_items()
-
-    def add_items(self):
-        for i,item in enumerate(self.value):
-            pass
-
-class ElementWidget(widgets.ContainerWidget):
-    lst = List(sync = True)
-    index = Any(sync = True)
-    value = Any(sync = True)
-
-    _elem_control = Type(widgets.Widget)
-
-    def __init__(self, *args, **kwargs):
-        super(ListWidget,self).__init__(*args, **kwargs)
-        self._callbacks = widgets.CallbackDispatcher()
-        self._elem_widget = self._elem_control(description = self.index)
-        traitlets.link((self,'value'),(self._elem_widget,'value'))
-        self._del_but = widgets.ButtonWidget()
-
-
-        children = [
-            self._elem_widget()
-
-        ]
